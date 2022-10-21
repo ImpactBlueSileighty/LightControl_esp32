3e3e6ed8a88d84679fbe1931db13d002a1833582
Igor <72380374+ImpactBlueSileighty@users.noreply.github.com>
2022-10-21 22:23:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
-// Подключаем библиотеку для работы с HTTP-протоколом
 #include <HTTPClient.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Wire.h>
 
-// Вводим имя и пароль точки доступа
 const char* ssid = "SSID_NETWORK";
 const char* password = "PASSWORD";
 
@@ -15,7 +13,6 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
 void setup() {
   Serial.begin(115200);
-  // делаем небольшую задержку на открытие монитора порта
   delay(5000);
   // подключаемся к Wi-Fi сети
   WiFi.begin(ssid, password);
@@ -32,17 +29,13 @@ void setup() {
 }
 
 void loop() {
-  // выполняем проверку подключения к беспроводной сети
   if ((WiFi.status() == WL_CONNECTED)) {
-    // создаем объект для работы с HTTP
     HTTPClient http;
-    // подключаемся к веб-странице
     http.begin("http://192.168.1.67:8080/api/light");
-    // делаем GET запрос
     int httpCode = http.GET();
-    // проверяем успешность запроса
+
     if (httpCode > 0) {
-      // выводим ответ сервера
+
       String payload = http.getString();
       
       display.clearDisplay();
@@ -55,7 +48,7 @@ void loop() {
     else {
       Serial.println("Ошибка HTTP-запроса");
     }
-    // освобождаем ресурсы микроконтроллера
+
     http.end();
   }
   delay(10000);

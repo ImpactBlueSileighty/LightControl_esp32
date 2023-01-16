f07766309142d092baad9fedabda0be6ac1c5a88
Igor <72380374+ImpactBlueSileighty@users.noreply.github.com>
2023-01-16 20:13:11 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,61 @@
+#include <WiFi.h>
 #include <HTTPClient.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Wire.h>
+#include "ArduinoJson.h"
 
-const char* ssid = "SSID_NETWORK";
-const char* password = "PASSWORD";
+const char* ssid = "ssid";
+const char* password = "pass";
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+String serverName = "http://192.168.1.71/api/light";
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
-
+const int relay = 23;
+ 
 void setup() {
-  Serial.begin(115200);
-  delay(5000);
-  // подключаемся к Wi-Fi сети
+  Serial.begin(9600);
+  pinMode(relay, OUTPUT);
+
   WiFi.begin(ssid, password);
+  
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting...");
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected");
+  
+  Serial.println("Connected to the WiFi network");
 
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
-    Serial.println("SSD1306 allocation failed");
-    for(;;);
-  }
 }
-
+ 
 void loop() {
-  if ((WiFi.status() == WL_CONNECTED)) {
-    HTTPClient http;
-    http.begin("http://192.168.1.67:8080/api/light");
-    int httpCode = http.GET();
 
-    if (httpCode > 0) {
-
-      String payload = http.getString();
+  if(WiFi.status()== WL_CONNECTED){
       
-      display.clearDisplay();
-      display.setTextSize(1);             
-      display.setTextColor(WHITE);        
-      display.setCursor(0,20);             
-      display.println(payload);
-      display.display();
-    }
-    else {
-      Serial.println("Ошибка HTTP-запроса");
-    }
+      HTTPClient client;
+      client.begin(serverName);
+      int httpCode = client.GET();
 
-    http.end();
+      if(httpCode > 0 ){
+        
+        String payload = client.getString();
+        Serial.println("1: " + payload);
+
+        StaticJsonDocument<50> doc;
+        deserializeJson(doc,payload);
+
+        String status = doc["status"];
+        Serial.println(status);
+        if(status == "false"){
+          digitalWrite(relay, LOW);
+        }else if(status == "true"){
+          digitalWrite(relay, HIGH);
+        }
+     
+      }else{
+        Serial.println("Error HTTP Request");
+      }
+      
+
+    
   }
-  delay(10000);
+  
+  delay(5000);
+
 }
